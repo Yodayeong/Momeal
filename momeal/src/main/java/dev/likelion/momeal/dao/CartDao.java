52e684e40c1942c1582ae92f48036dad6a029d26
yodayeong <duekdud01@naver.com>
2023-08-16 23:56:11 +0900

    feat: CartDao CreateCart 구현

--- momeal/src/main/java/dev/likelion/momeal/dao/CartDao.java
+++ momeal/src/main/java/dev/likelion/momeal/dao/CartDao.java
@@ -2,7 +2,9 @@ package dev.likelion.momeal.dao;
 
 import dev.likelion.momeal.entity.CartEntity;
 import dev.likelion.momeal.entity.MenuEntity;
+import dev.likelion.momeal.entity.UserEntity;
 import dev.likelion.momeal.repository.CartRepository;
+import dev.likelion.momeal.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,14 +14,22 @@ import org.springframework.stereotype.Repository;
 public class CartDao {
     private static final Logger logger = LoggerFactory.getLogger(CartDao.class);
     private final CartRepository cartRepository;
+    private final UserRepository userRepository;
 
     public CartDao(
-            @Autowired CartRepository cartRepository
+            @Autowired CartRepository cartRepository,
+            @Autowired UserRepository userRepository
     ) {
         this.cartRepository = cartRepository;
+        this.userRepository = userRepository;
     }
 
     public void createCart(int price, int quantity, String email) {
-        
+        UserEntity user = userRepository.findByEmail(email);
+
+        CartEntity cartEntity = new CartEntity();
+        cartEntity.setPrice(price);
+        cartEntity.setQuantity(quantity);
+        cartEntity.setUserEntity(user);
     }
 }
--- momeal/src/main/java/dev/likelion/momeal/repository/CartRepository.java
+++ momeal/src/main/java/dev/likelion/momeal/repository/CartRepository.java
@@ -1,7 +1,9 @@
 package dev.likelion.momeal.repository;
 
 import dev.likelion.momeal.entity.CartEntity;
+import dev.likelion.momeal.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CartRepository extends CrudRepository<CartEntity, Long> {
+    UserEntity findByEmail(String email);
 }
