20d4804d02f1fc5af15dcacadda39de8acded56f
yodayeong <duekdud01@naver.com>
2023-08-17 00:09:25 +0900

    fix: CartDao createCart 수정

--- momeal/src/main/java/dev/likelion/momeal/dao/CartDao.java
+++ momeal/src/main/java/dev/likelion/momeal/dao/CartDao.java
@@ -5,31 +5,23 @@ import dev.likelion.momeal.entity.MenuEntity;
 import dev.likelion.momeal.entity.UserEntity;
 import dev.likelion.momeal.repository.CartRepository;
 import dev.likelion.momeal.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class CartDao {
     private static final Logger logger = LoggerFactory.getLogger(CartDao.class);
     private final CartRepository cartRepository;
     private final UserRepository userRepository;
 
-    public CartDao(
-            @Autowired CartRepository cartRepository,
-            @Autowired UserRepository userRepository
-    ) {
-        this.cartRepository = cartRepository;
-        this.userRepository = userRepository;
-    }
-
     public void createCart(int price, int quantity, String email) {
-        UserEntity user = userRepository.findByEmail(email);
-
         CartEntity cartEntity = new CartEntity();
         cartEntity.setPrice(price);
         cartEntity.setQuantity(quantity);
-        cartEntity.setUserEntity(user);
+        cartEntity.setUserEntity(userRepository.findByEmail(email));
     }
 }
